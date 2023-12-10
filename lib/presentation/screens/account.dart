cc377e394e3b168fb1b09892370da3b58b0479d8
Raghvendra Mishra <raghvendramishra2002@gmail.com>
2023-12-10 14:09:38 +0530

    Added section to Account; Fixed Button UI

--- lib/presentation/screens/account.dart
+++ lib/presentation/screens/account.dart
@@ -2,11 +2,14 @@
 
 import 'package:area_51/business_logic/cubits/themeCubit/theme_cubit.dart';
 import 'package:area_51/constants/colors.dart';
+import 'package:area_51/constants/icons.dart';
 import 'package:area_51/data/models/user.dart';
-import 'package:carbon_icons/carbon_icons.dart';
+import 'package:area_51/data/repositories/products_Repo.dart';
+import 'package:area_51/presentation/widgets/homeWidgets/homeScreenLists.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
 
 class Account extends StatefulWidget {
   Account(
@@ -39,24 +42,22 @@ class _AccountState extends State<Account> {
       AspectRatio(
           aspectRatio: 2,
           child: Align(
-              alignment: Alignment(-1, 0),
-              child: Padding(
-                  padding: EdgeInsets.only(left: 40),
-                  child: RichText(
-                      text: TextSpan(children: [
-                    TextSpan(
-                        text: "Hello,",
-                        style: GoogleFonts.poppins(
-                            fontSize: 31,
-                            fontWeight: FontWeight.bold,
-                            color: widget.theme.oppAccent)),
-                    TextSpan(
-                        text: "Siddhartha Mishra",
-                        style: GoogleFonts.poppins(
-                            fontSize: 23, color: widget.theme.oppAccent)),
-                  ]))))),
+              alignment: Alignment(0, 0),
+              child: RichText(
+                  text: TextSpan(children: [
+                TextSpan(
+                    text: "Hello,",
+                    style: GoogleFonts.poppins(
+                        fontSize: 31,
+                        fontWeight: FontWeight.bold,
+                        color: widget.theme.oppAccent)),
+                TextSpan(
+                    text: "Siddhartha Mishra",
+                    style: GoogleFonts.poppins(
+                        fontSize: 23, color: widget.theme.oppAccent)),
+              ])))),
       AspectRatio(
-          aspectRatio: 2,
+          aspectRatio: 2.2,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -64,33 +65,39 @@ class _AccountState extends State<Account> {
                   padding: EdgeInsets.only(left: 20),
                   child: Text("Theme",
                       style: GoogleFonts.poppins(
-                          fontSize: 25,
+                          fontSize: 24,
                           fontWeight: FontWeight.bold,
                           color: widget.theme.oppAccent))),
               Padding(
-                  padding: EdgeInsets.only(left: 45, right: 45, top: 30),
+                  padding: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width * 0.1,
+                      right: MediaQuery.of(context).size.width * 0.1,
+                      top: 20),
                   child: AspectRatio(
-                      aspectRatio: 4.8,
+                      aspectRatio: 5.2,
                       child: ElevatedButton(
                           onPressed: () {
                             BlocProvider.of<ThemeCubit>(context).changeTheme();
                           },
                           style: ElevatedButton.styleFrom(
                               elevation: 0,
-                              backgroundColor: Colors.transparent,
+                              backgroundColor: widget.theme.oppAccent,
                               shape: RoundedRectangleBorder(
                                   side: BorderSide(
-                                      color: widget.theme.oppAccent, width: 2),
+                                      color: widget.theme.mainAccent, width: 2),
                                   borderRadius: BorderRadius.circular(60))),
                           child: Row(
                             children: [
                               Spacer(),
-                              Icon(
-                                widget.state.themeIsLight
-                                    ? CarbonIcons.asleep_filled
-                                    : CarbonIcons.awake,
-                                color: widget.theme.oppAccent,
-                                size: 35,
+                              Padding(
+                                padding: EdgeInsets.only(right: 5),
+                                child: !widget.state.themeIsLight
+                                    ? Iconify(light_Mode,
+                                        size: 30,
+                                        color: widget.theme.mainAccent)
+                                    : Iconify(dark_Mode,
+                                        size: 30,
+                                        color: widget.theme.mainAccent),
                               ),
                               Padding(
                                   padding: EdgeInsets.only(left: 3),
@@ -99,7 +106,7 @@ class _AccountState extends State<Account> {
                                         ? "Dark Mode"
                                         : "Light Mode",
                                     style: GoogleFonts.poppins(
-                                        color: widget.theme.oppAccent,
+                                        color: widget.theme.mainAccent,
                                         fontSize: 19,
                                         fontWeight: FontWeight.w500),
                                   )),
@@ -107,7 +114,51 @@ class _AccountState extends State<Account> {
                             ],
                           )))),
             ],
-          ))
+          )),
+      Padding(
+        padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Your Wishlist",
+                    style: GoogleFonts.poppins(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: widget.theme.oppAccent)),
+                GestureDetector(
+                  onTap: () {},
+                  child: Row(
+                    children: [
+                      Text("More",
+                          style: GoogleFonts.poppins(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: widget.theme.oppAccent)),
+                      Iconify(ios_forward,
+                          color: widget.theme.oppAccent, size: 20)
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.only(bottom: 30),
+        child: AspectRatio(
+          aspectRatio: 1.5,
+          child: HomeScreenLists(
+            theme: widget.theme,
+            title: "Recommendation",
+            removeTitle: true,
+            products: ProductList(),
+          ),
+        ),
+      ),
     ]);
   }
 }
--- lib/presentation/widgets/homeWidgets/homeScreenLists.dart
+++ lib/presentation/widgets/homeWidgets/homeScreenLists.dart
@@ -10,11 +10,17 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:area_51/data/repositories/products_Repo.dart';
 
 class HomeScreenLists extends StatefulWidget {
-  HomeScreenLists({super.key, required this.title, required this.theme, required this.products});
+  HomeScreenLists(
+      {super.key,
+      required this.title,
+      required this.theme,
+      required this.products,
+      this.removeTitle = false});
 
   String title;
   LightMode theme;
   ProductList products;
+  bool removeTitle;
 
   @override
   State<HomeScreenLists> createState() => _HomeScreenListsState();
@@ -32,18 +38,20 @@ class _HomeScreenListsState extends State<HomeScreenLists> {
   @override
   Widget build(BuildContext context) {
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      Padding(
-          padding: EdgeInsets.only(left: 23, bottom: 30),
-          child: AutoSizeText(widget.title,
-              maxFontSize: 24,
-              minFontSize: 17,
-              style: GoogleFonts.poppins(
-                  color: widget.theme.oppAccent,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24))),
+      widget.removeTitle
+          ? Container()
+          : Padding(
+              padding: EdgeInsets.only(left: 23, bottom: 30),
+              child: AutoSizeText(widget.title,
+                  maxFontSize: 24,
+                  minFontSize: 17,
+                  style: GoogleFonts.poppins(
+                      color: widget.theme.oppAccent,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24))),
       Expanded(
           child: ListView.builder(
-            key: PageStorageKey<String>(widget.title),
+              key: PageStorageKey<String>(widget.title),
               scrollDirection: Axis.horizontal,
               itemCount: productsList.length,
               itemBuilder: ((context, index) {
