e1038b79868b9266e7f1a81199a0b5cb475a08f5
Raghvendra Mishra <raghvendramishra2002@gmail.com>
2023-12-13 17:22:21 +0530

    Adding Firebase again

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:area_51/presentation/pages/login.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(LogIn());
 }
